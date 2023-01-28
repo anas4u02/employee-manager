55f85d7f1a31a688b65ccec6cc4a42489ce2cae7
Mohammad Anas <anas4u02@gmail.com>
2023-01-29 00:50:49 +0530

    CRUD APIs made

---
+++ src/main/java/tech/personal/employeemanager/repo/EmployeeRepo.java
@@ -0,0 +1,18 @@
+package tech.personal.employeemanager.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+import tech.personal.employeemanager.model.Employee;
+
+import java.util.Optional;
+
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+    @Transactional
+    @Modifying
+    @Query("delete from Employee e")
+    void deleteEmployeeById(Long id);
+
+    Optional<Employee> findEmployeeById(Long id);
+}
