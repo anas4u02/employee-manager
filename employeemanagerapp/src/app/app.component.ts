ba76b262f7d09e3d8e6b5cb92de2841a5b3a6c85
Mohammad Anas <anas4u02@gmail.com>
2023-01-30 19:17:33 +0530

    app component configured along with environment

--- employeemanagerapp/src/app/app.component.ts
+++ employeemanagerapp/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from './employee';
+import { EmployeeService } from './employee.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'employeemanagerapp';
+export class AppComponent implements OnInit{
+  public employees: Employee[];
+  
+  constructor(private employeeService: EmployeeService){
+    this.employees =[]; //initialization to avoid the compiler error. Done by me, subject to review
+  }
+  ngOnInit() {
+    this.getEmployees();
+  }
+
+  public getEmployees(): void {
+    this.employeeService.getEmployees().subscribe(
+      (response: Employee[]) => {
+        this.employees = response;
+      },
+      (error: HttpErrorResponse)=>{
+        alert(error.message);
+      }
+      );
+  }
 }
